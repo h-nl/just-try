682777d218c96077cd1c2167f485a9806e010067
xinle <15112415709@163.com>
2022-07-24 19:17:30 +0800

    hnl first commit.

---
+++ main.go
@@ -0,0 +1,20 @@
+/*
+ * @Author: xinle 15112415709@163.com
+ * @Date: 2022-07-22 14:25:43
+ * @LastEditors: xinle 15112415709@163.com
+ * @LastEditTime: 2022-07-24 19:16:22
+ * @FilePath: \go-projects\sample-go\main.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	name := "Go Developers"
+	fmt.Println("Azure for", name)
+
+	fmt.Println("this is H-NL comes.")
+	fmt.Println("this is H-NL comes again.")
+}
